89e57ae89ff6415555f0856cc8981fd27c722536
nikhil <nikhilcm42@gmail.com>
2016-11-03 22:44:48 -0700

    formatting

--- bot.js
+++ bot.js
@@ -7,23 +7,16 @@ processedGrammar.addModifiers(tracery.baseEngModifiers);
 var tweet = processedGrammar.flatten("#origin#");
 //console.log(tweet);
 
-
 var Twit = require('twit');
 
-
-var T = new Twit(
-{
-    consumer_key:         process.env.TWITTER_CONSUMER_KEY
-  , consumer_secret:      process.env.TWITTER_CONSUMER_SECRET
-  , access_token:         process.env.TWITTER_ACCESS_TOKEN
-  , access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
-}
-);
+var T = new Twit({
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY, 
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET, 
+    access_token:         process.env.TWITTER_ACCESS_TOKEN, 
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 
 T.post('statuses/update', { status: tweet }, function(err, data, response) {
   //console.log(data)
 })
-
-
-
